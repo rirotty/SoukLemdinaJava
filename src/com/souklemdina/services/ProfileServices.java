042d706cd48511fa6345fee1ad396922953415c1
Ramy Kader <ramy.kader@esprit.tn>
2018-03-13 23:06:16 +0100

    TestPackagesAdded - Commit

--- src/com/souklemdina/services/ProfileServices.java
+++ src/com/souklemdina/services/ProfileServices.java
@@ -5,10 +5,34 @@
  */
 package com.souklemdina.services;
 
+import com.souklemdina.entities.Profile;
+import com.souklemdina.interfaces.IProfileServices;
+import java.util.List;
+
 /**
  *
  * @author ramyk
  */
-public class ProfileServices {
+public class ProfileServices implements IProfileServices{
+
+    @Override
+    public void create(Profile p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Profile> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Profile p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Profile p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
